a2a298a50935ade105a756fe191595f192b987f1
Dharmik2005-Godalka <dharmikgodalka@gmail.com>
2025-11-30 17:40:36 +0530

    Lab_13
    To create and traverse the Binary Tree.

---
+++ LAB_13/To create and traverse the Binary Tree.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = right = nullptr;
+    }
+};
+
+class BinaryTree {
+public:
+    Node* root;
+
+    BinaryTree() {
+        root = nullptr;
+    }
+
+    Node* insert(Node* node, int value) {
+        if (node == nullptr)
+            return new Node(value);
+
+        if (value < node->data)
+            node->left = insert(node->left, value);
+        else
+            node->right = insert(node->right, value);
+
+        return node;
+    }
+
+    void preOrder(Node* node) {
+        if (node == nullptr) return;
+        cout << node->data << " ";
+        preOrder(node->left);
+        preOrder(node->right);
+    }
+
+    void inOrder(Node* node) {
+        if (node == nullptr) return;
+        inOrder(node->left);
+        cout << node->data << " ";
+        inOrder(node->right);
+    }
+
+    void postOrder(Node* node) {
+        if (node == nullptr) return;
+        postOrder(node->left);
+        postOrder(node->right);
+        cout << node->data << " ";
+    }
+};
+
+int main() {
+    BinaryTree tree;
+
+    tree.root = tree.insert(tree.root, 50);
+    tree.root = tree.insert(tree.root, 30);
+    tree.root = tree.insert(tree.root, 70);
+    tree.root = tree.insert(tree.root, 20);
+    tree.root = tree.insert(tree.root, 40);
+    tree.root = tree.insert(tree.root, 60);
+    tree.root = tree.insert(tree.root, 80);
+
+    cout << "Pre-Order Traversal: ";
+    tree.preOrder(tree.root);
+    cout << endl;
+
+    cout << "In-Order Traversal: ";
+    tree.inOrder(tree.root);
+    cout << endl;
+
+    cout << "Post-Order Traversal: ";
+    tree.postOrder(tree.root);
+    cout << endl;
+
+    return 0;
+}
+
